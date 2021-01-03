bd1d3a8e9f6abf283fc7386664e465be49bb2adb
= <=>
2021-01-03 09:34:23 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Django Role based authentication
---
+++ auth_api/models.py
@@ -0,0 +1,51 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils import timezone
+
+from .managers import CustomUserManager
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+
+    # These fields tie to the roles!
+    ADMIN = 1
+    MANAGER = 2
+    EMPLOYEE = 3
+
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (MANAGER, 'Manager'),
+        (EMPLOYEE, 'Employee')
+    )
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    email = models.EmailField(unique=True,verbose_name="email")
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True, default=7)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    created_by = models.EmailField()
+    modified_by = models.EmailField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.first_name+" "+self.last_name
